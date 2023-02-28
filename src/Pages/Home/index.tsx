865d69d177438c59d9da05e6e84f4d04fac7ecb3
Brian Poblete <brianmp.dev@gmail.com>
2023-02-28 01:21:49 -0500

    cleanup and style back btn

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,30 +1,34 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './Home.module.css';
 import { A } from '@solidjs/router';
+import styles from './Home.module.css';
+import logo from './logo.svg';
 
 const Home = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <A
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </A>
-        <A class={styles.link} href="/OrderSummaryComponent">
-          Order Summary Component
-        </A>
-        <A class={styles.link} href="/QRCodeComponent">
-          QR Code Component
-        </A>
-      </header>
-    </div>
+    <header class={styles.header}>
+      <img src={logo} class={styles.logo} alt="logo" />
+      <A
+        class={styles.link}
+        href="https://github.com/solidjs/solid"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn Solid
+      </A>
+      <A
+        class={styles.link}
+        href="https://github.com/Brian-Pob/frontend-mentor/tree/master"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        My GitHub Repo
+      </A>
+      <A class={styles.link} href="/OrderSummaryComponent">
+        Order Summary Component
+      </A>
+      <A class={styles.link} href="/QRCodeComponent">
+        QR Code Component
+      </A>
+    </header>
   );
 };
 
