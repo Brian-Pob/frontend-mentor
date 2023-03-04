46ec3f21ade6f60257ae60f3423df464874ec419
Brian-Pob <brianpoblete77777@gmail.com>
2023-03-04 14:40:27 -0500

    more style adjustments

--- src/Challenges/OrderSummaryComponent/index.tsx
+++ src/Challenges/OrderSummaryComponent/index.tsx
@@ -1,29 +1,27 @@
-import { Component, createEffect } from 'solid-js';
-import { Button, Card, PlanSelection } from './components';
+import { Component, createEffect } from "solid-js";
+import { Button, Card, PlanSelection } from "./components";
 
-import styles from './index.module.css';
-import image from './assets/illustration-hero.svg';
-import iconMusic from './assets/icon-music.svg';
+import styles from "./index.module.css";
+import image from "./assets/illustration-hero.svg";
+import iconMusic from "./assets/icon-music.svg";
 const OrderSummaryComponent: Component = () => {
   return (
     <>
       <main class={styles.OrderSummaryComponent}>
-        <section>
-          <Card
-            header="Order Summary"
-            body="You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!"
-            img_url={image}
-          >
-            <PlanSelection
-              planTitle="Annual Plan"
-              planPrice="$59.99"
-              planPeriod="year"
-              img_url={iconMusic}
-            />
-            <Button text="Proceed to Payment" />
-            <Button text="Cancel Order" btnType="secondary" />
-          </Card>
-        </section>
+        <Card
+          header="Order Summary"
+          body="You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!"
+          img_url={image}
+        >
+          <PlanSelection
+            planTitle="Annual Plan"
+            planPrice="$59.99"
+            planPeriod="year"
+            img_url={iconMusic}
+          />
+          <Button text="Proceed to Payment" />
+          <Button text="Cancel Order" btnType="secondary" />
+        </Card>
       </main>
     </>
   );
