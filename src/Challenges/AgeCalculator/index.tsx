254ac5f0c0b2957fe390d186c7de3d2f95009dfc
Brian-Pob <brianpoblete77777@gmail.com>
2023-08-08 22:17:35 -0400

    implemented validation

--- src/Challenges/AgeCalculator/index.tsx
+++ src/Challenges/AgeCalculator/index.tsx
@@ -1,61 +1,95 @@
-import { createEffect, createSignal } from 'solid-js';
+import { For, Show, createEffect, createRenderEffect, createSignal, onMount } from 'solid-js';
 import dayjs from 'dayjs';
+import isLeapYear from 'dayjs/plugin/isLeapYear';
+import './index.css';
 
 export default function AgeCalculator() {
+  dayjs.extend(isLeapYear);
   const [year, setYear] = createSignal(1970);
   const [month, setMonth] = createSignal(1);
   const [day, setDay] = createSignal(1);
-  const [hours, setHours] = createSignal(0);
-  const [minutes, setMinutes] = createSignal(0);
-  const [seconds, setSeconds] = createSignal(0);
+
+  const [errors, setErrors] = createSignal({
+    year: '',
+    month: '',
+    day: '',
+    future: '',
+  });
 
   const [elapsed, setElapsed] = createSignal({
-    years: 0,
-    months: 0,
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
+    years: '0',
+    months: '0',
+    days: '0',
   });
 
   const calculateAge = () => {
-    // TODO: add validation
-
-    setYear(() => Number((document.querySelector('input[name="year"]') as HTMLInputElement).value));
-    setMonth(() =>
-      Number((document.querySelector('input[name="month"]') as HTMLInputElement).value)
+    const yearInput = Number(
+      (document.querySelector('input[name="year"]') as HTMLInputElement).value
     );
-    setDay(() => Number((document.querySelector('input[name="day"]') as HTMLInputElement).value));
-    setHours(() =>
-      Number((document.querySelector('input[name="hour"]') as HTMLInputElement).value)
+    const monthInput = Number(
+      (document.querySelector('input[name="month"]') as HTMLInputElement).value
     );
-    setMinutes(() =>
-      Number((document.querySelector('input[name="minute"]') as HTMLInputElement).value)
-    );
-    setSeconds(() =>
-      Number((document.querySelector('input[name="second"]') as HTMLInputElement).value)
+    const dayInput = Number(
+      (document.querySelector('input[name="day"]') as HTMLInputElement).value
     );
 
-    const birthday = dayjs(`${year()}-${month()}-${day()}`);
-    const now = dayjs();
-    const diff = now.diff(birthday, 'second');
+    if (yearInput < 1900 || yearInput > dayjs().year()) {
+      setErrors((prev) => ({ ...prev, year: 'Year must be between 1900 and current year' }));
+    } else if (yearInput > 1900 && yearInput < dayjs().year()) {
+      setErrors((prev) => ({ ...prev, year: '' }));
+    }
 
-    setElapsed({
-      years: now.diff(birthday, 'year'),
-      months: now.diff(birthday, 'month') % 12,
-      days: now.diff(birthday, 'day') % 30,
-      hours: now.diff(birthday, 'hour') % 24,
-      minutes: now.diff(birthday, 'minute') % 60,
-      seconds: now.diff(birthday, 'second') % 60,
-    });
+    if (monthInput < 1 || monthInput > 12) {
+      setErrors((prev) => ({ ...prev, month: 'Month must be between 1 and 12' }));
+    } else if (monthInput > 0 && monthInput < 13) {
+      setErrors((prev) => ({ ...prev, month: '' }));
+    }
+
+    // Handle leap years and months with 30 days
+    if (dayjs(yearInput).isLeapYear() && monthInput === 2 && dayInput > 29) {
+      setErrors((prev) => ({ ...prev, day: 'February has 29 days this year' }));
+    } else if (!dayjs(yearInput).isLeapYear() && monthInput === 2 && dayInput > 28) {
+      setErrors((prev) => ({ ...prev, day: 'February has 28 days this year' }));
+    } else if ([4, 6, 9, 11].includes(monthInput) && dayInput > 30) {
+      setErrors((prev) => ({ ...prev, day: 'This month has 30 days' }));
+    } else if (dayInput < 1 || dayInput > 31) {
+      setErrors((prev) => ({ ...prev, day: 'Day must be between 1 and 31' }));
+    } else if (dayInput > 0 && dayInput < 32) {
+      setErrors((prev) => ({ ...prev, day: '' }));
+    }
+
+    if (!errors().year && !errors().month && !errors().day) {
+      if (dayjs(`${yearInput}-${monthInput}-${dayInput}`).isAfter(dayjs())) {
+        setErrors((prev) => ({ ...prev, future: 'Birthday cannot be in the future' }));
+      } else {
+        setErrors((prev) => ({ ...prev, future: '' }));
+      }
+    }
+
+    if (errors().year || errors().month || errors().day || errors().future) {
+      setElapsed({
+        years: '--',
+        months: '--',
+        days: '--',
+      });
+    } else {
+      const birthday = dayjs(`${year()}-${month()}-${day()}`);
+
+      const now = dayjs();
+
+      setElapsed({
+        years: now.diff(birthday, 'year').toString(),
+        months: (now.diff(birthday, 'month') % 12).toString(),
+        days: (now.diff(birthday, 'day') % 30).toString(),
+      });
+    }
+    setYear(yearInput);
+    setMonth(monthInput);
+    setDay(dayInput);
   };
 
-  // update every second
-  createEffect(() => {
-    const interval = setInterval(() => {
-      calculateAge();
-    }, 1000);
-    return () => clearInterval(interval);
+  onMount(() => {
+    calculateAge();
   });
 
   return (
@@ -63,20 +97,43 @@ export default function AgeCalculator() {
       <div>
         <h1>Age Calculator</h1>
         <p>Enter your birthday to calculate your age.</p>
-        <form>
-          <label for="year"></label>
-          <input type="number" placeholder="Year" name="year" value={year()} />
-          <label for="month"></label>
-          <input type="number" placeholder="Month" name="month" value={month()} />
-          <label for="day"></label>
-          <input type="number" placeholder="Day" name="day" value={day()} />
-          <label for="hour"></label>
-          <input type="number" placeholder="Hour" name="hour" value={hours()} />
-          <label for="minute"></label>
-          <input type="number" placeholder="Minute" name="minute" value={minutes()} />
-          <label for="second"></label>
-          <input type="number" placeholder="Second" name="second" value={seconds()} />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            calculateAge();
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              calculateAge();
+            }
+          }}
+        >
+          <div>
+            <label for="year">Year</label>
+            <input type="number" placeholder="Year" name="year" value={year()} />
+          </div>
+
+          <div>
+            <label for="month">Month</label>
+            <input type="number" placeholder="Month" name="month" value={month()} />
+          </div>
+
+          <div>
+            <label for="day">Day</label>
+            <input type="number" placeholder="Day" name="day" value={day()} />
+          </div>
+
+          <button type="submit" onClick={() => calculateAge()}>
+            Calculate
+          </button>
         </form>
+
+        <Show when={errors().year || errors().month || errors().day || errors().future}>
+          <div class="errors">
+            <For each={Object.values(errors())}>{(error) => <p>{error}</p>}</For>
+          </div>
+        </Show>
+
         <div>
           <p>
             <span>{elapsed().years}</span> years
@@ -86,15 +143,6 @@ export default function AgeCalculator() {
           </p>
           <p>
             <span>{elapsed().days}</span> days
-          </p>
-          <p>
-            <span>{elapsed().hours}</span> hours
-          </p>
-          <p>
-            <span>{elapsed().minutes}</span> minutes
-          </p>
-          <p>
-            <span>{elapsed().seconds}</span> seconds
           </p>
         </div>
       </div>
